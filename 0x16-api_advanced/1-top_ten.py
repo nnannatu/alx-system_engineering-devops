5a4b88a9317ca0d2d72e651ad880aef546201609
nnanna <atunnannasamson@gmail.com>
2024-05-14 15:24:41 +0100

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,17 +5,21 @@ import requests
 
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        'User-Agent': 'MyRedditAPI/0.1
+        (Python3.8; Linux; Ubuntu 20.04; Developer: johndoe@example.com)'
     }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        # Check if the response status code is 200 (OK)
+        if response.status_code == 200:
+            data = response.json()
+            posts = data['data']['children']
+            for post in posts:
+                print(post['data']['title'])
+        else:
+            print(None)
+    except requests.RequestException:
+        print(None)
